adb4ca8f8a0aeb62a3d2a06f94814db1f8b410a9
okubis <daniel.slunecko@gmail.com>
2016-05-14 12:06:10 +0200

    genesis

--- src/individual/IndividualInterface.java
+++ src/individual/IndividualInterface.java
@@ -3,11 +3,12 @@ package individual;
 /**
  * Created by okubis on 5/14/16.
  */
-public interface IndividualInterface {
+public interface IndividualInterface extends Comparable {
 
     // has to have this getter
     abstract ANNInterface getGenome();
 
     // has to have this getter
     abstract Double getFitness();
+
 }
