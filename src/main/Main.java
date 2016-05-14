86ff4e62748a9555cddefdd2775d4416c86d847d
okubis <daniel.slunecko@gmail.com>
2016-05-14 11:39:09 +0200

    genesis

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by okubis on 5/14/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //TODO: this will be the main class
+    }
+}
