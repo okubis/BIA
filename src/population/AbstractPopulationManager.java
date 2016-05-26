4dbe924eb4f809264efe9756f1c55b7e41995235
okubis <daniel.slunecko@gmail.com>
2016-05-26 18:51:38 +0200

    PopulationManager

--- src/population/AbstractPopulationManager.java
+++ src/population/AbstractPopulationManager.java
@@ -6,8 +6,8 @@ import java.util.Random;
  * Created by okubis on 5/26/16.
  */
 public abstract class AbstractPopulationManager {
-    public abstract void init_population(Random rand);
-    public abstract Population nextGeneration(Population p,Random rand);
+    public abstract void init_population(Random rand, int populationSize);
+    public abstract Population nextGeneration(Population p,Random rand, int populationSize);
 
 
 
--- src/population/PopulationManager.java
+++ src/population/PopulationManager.java
@@ -6,11 +6,11 @@ import java.util.Random;
  * Created by okubis on 5/26/16.
  */
 public class PopulationManager extends AbstractPopulationManager {
-    public void init_population(Random rand) {
+    public void init_population(Random rand, int populationSize) {
 
     }
 
-    public Population nextGeneration(Population p, Random rand) {
+    public Population nextGeneration(Population p, Random rand, int populationSize) {
         return null;
     }
 }
