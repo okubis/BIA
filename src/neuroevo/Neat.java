03013c6591c837e64132e107c712787d56b81677
rindtedu <eda.rindt@gmail.com>
2016-05-27 23:53:22 +0200

    stub of species division

--- src/neuroevo/Neat.java
+++ src/neuroevo/Neat.java
@@ -1,5 +1,13 @@
 package neuroevo;
 
+import java.util.ArrayList;
+
+import com.layer.SocketConnectionParameters;
+
+import population.*;
+import individual.*;
+import ann.*;
+
 /**
  * Created by okubis on 5/26/16.
  */
@@ -12,4 +20,11 @@ public class Neat {
     //TODO: metodka pro krizeni
     //TODO: metodka pro mutaci
     //TODO: metodka pro navrat nejlepsiho vysledku celeho behu NEATu
+
+
+	
+    private void divideIntoSpecies(){
+    	ArrayList<ArrayList<Integer>> species = new ArrayList<ArrayList<Integer>>();
+    	
+    }
 }
