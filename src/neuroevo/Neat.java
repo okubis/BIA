5dd79627e22f38574799385dc2ee914ad21090bd
okubis <daniel.slunecko@gmail.com>
2016-05-27 23:49:48 +0200

    NEAT: promenne

--- src/neuroevo/Neat.java
+++ src/neuroevo/Neat.java
@@ -1,10 +1,23 @@
 package neuroevo;
 
+import com.layer.SocketConnectionParameters;
+import population.AbstractPopulationManager;
+import population.Population;
+import population.PopulationManager;
+
+import java.util.ArrayList;
+
 /**
  * Created by okubis on 5/26/16.
  */
 public class Neat {
+    private Population population;
+    private PopulationManager populationManager;
+    private ArrayList<ArrayList<Integer>> species; // change type as you wish
+
+
     //TODO: constructor;
+
     //TODO: metoda Evolve (i.e: hlavni metoda NEAT volana z main)
     //TODO: metodka pro rozrazeni do druhu;
     //TODO: metodka pro vyber turnajem - Druh
