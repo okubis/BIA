c43a9c8fac979c613bf2aa688f97febee1213538
okubis <daniel.slunecko@gmail.com>
2016-05-27 23:21:32 +0200

    TODO:NEAT

--- src/neuroevo/Neat.java
+++ src/neuroevo/Neat.java
@@ -4,5 +4,12 @@ package neuroevo;
  * Created by okubis on 5/26/16.
  */
 public class Neat {
-    private int populationSize;
+    //TODO: constructor;
+    //TODO: metoda Evolve (i.e: hlavni metoda NEAT volana z main)
+    //TODO: metodka pro rozrazeni do druhu;
+    //TODO: metodka pro vyber turnajem - Druh
+    //TODO: metodka pro vyber turnajem1 - Jedinec
+    //TODO: metodka pro krizeni
+    //TODO: metodka pro mutaci
+    //TODO: metodka pro navrat nejlepsiho vysledku celeho behu NEATu
 }
